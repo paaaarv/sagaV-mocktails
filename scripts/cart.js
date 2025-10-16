10dbae8d7a260f0095b5638354cbc40dcb35b4fe
Parvathi Kumar <parvathikumar92@gmail.com>
2025-10-15 22:33:56 -0500

    cart total, desktop styling cart component

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,9 +1,6 @@
 let shoppingCart = JSON.parse(localStorage.getItem('saga-v-cart')) || []; 
+let cartQuantity = 0; 
 
-
-
-// dynamic cart quantity 
-$("#cart-quantity").text(cart.length); 
 
 
 //add items dynamically to page from cart 
@@ -11,27 +8,42 @@ const showCart = () => {
     for (let i=0; i < shoppingCart.length; i++){
         let product = shoppingCart[i]; 
         let mocktail = mocktailsArray.find(drink => drink.id == product.id); 
+        cartQuantity += product.quantity; 
 
         $("#cart-items").append(`
-            <div class="cart-item">
-                <img src=${mocktail.img} class="cart-img" alt= "${mocktail.name}"/>
-                <div class="cart-info">
-                    <h3>${mocktail.name}</h3>
-                    <div class="qty">
-                        <button class="qty-btn">−</button>
-                        <input type="number" class="qty-input" value= ${product.quantity}  min="1" />
-                        <button class="qty-btn">+</button>
+            <div>
+                <div class="cart-item">
+                    <img src=${mocktail.img} class="cart-img" alt= "${mocktail.name}"/>
+                    <div class="cart-info">
+                        <h3>${mocktail.name}</h3>
+                        <div class="qty">
+                            <button class="qty-btn">−</button>
+                            <input type="number" class="qty-input" value= ${product.quantity}  min="1" />
+                            <button class="qty-btn">+</button>
+                        </div>
+                    </div>
+                    <div class="cart-meta">
+                        <button class="trash" aria-label="Remove ${mocktail.name}">
+                            <i class="fa-regular fa-trash-can trash-can" style="color: #cfb482;"></i>
+                        </button>
+                        <p>$${mocktail.price} </p>
                     </div>
                 </div>
-                <div class="cart-meta">
-                    <button class="trash" aria-label="Remove ${mocktail.name}">
-                         <i class="fa-regular fa-trash-can" style="color: #cfb482;"></i>
-                    </button>
-                    <p>$${mocktail.price} </p>
-                </div>
+                <hr>
             </div>`
         )
     }
 }
 
+
+const showOrderSummary = () => { 
+
+}
+
+
+
+// add document loaded event listener 
+
 showCart();
+// dynamic cart quantity 
+$("#cart-quantity").text(cartQuantity); 
